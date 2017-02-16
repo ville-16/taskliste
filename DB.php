34caa60b190de3373b5fa8842f77eeb297c39985
Fabian Schlegel <fschlegel@gmx.ch>
2017-02-16 21:10:23 +0100

    commit class DB

---
+++ DB.php
@@ -0,0 +1,28 @@
+<?php
+
+class DB {
+    private static $db = null;
+    public static function get(){
+
+        if (DB::$db==null){
+
+        $servername = "localhost";
+        $db_name = "TODO_APP";
+        $username = "f.schlegel";
+        $password = "hill-96";
+
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$db_name;charset=utf8", $username, $password);
+            //set the PDO error mode executation
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            DB::$db = $conn;
+        }
+        catch (PDOException $e) {
+            die("Connection faild: " . $e->getMessage());
+        }
+        }
+        return DB::$db;
+    }
+}
